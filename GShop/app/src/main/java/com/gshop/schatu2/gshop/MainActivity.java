c9670e384382af281c40dc0936f9e0f7f27009fd
yashasvi <yashasvi.girdhar@walmart.com>
2016-04-02 23:04:53 +0530

    adding logging functionality

---
+++ GShop/app/src/main/java/com/gshop/schatu2/gshop/MainActivity.java
@@ -0,0 +1,92 @@
+package com.gshop.schatu2.gshop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+
+/**
+ * Created by dev-ygirdha on 4/2/2016.
+ */
+public class MainActivity extends AppCompatActivity{
+    Button b1,b2;
+    EditText ed1,ed2;
+
+    TextView tx1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        b1=(Button)findViewById(R.id.button);
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+
+       // b2=(Button)findViewById(R.id.button2);
+        tx1=(TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((ed1.getText().toString().equals("darshnik") && ed2.getText().toString().equals("walmart"))) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, RecyclerViewActivity.class));
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+    }
+
+   // @Override
+  /*  public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+}
